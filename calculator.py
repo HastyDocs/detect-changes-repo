64c7ba64e77ed08af9eee59b687751112cbaa155
mmajde <marek.majde@gmail.com>
2021-12-08 10:32:46 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
-float add(float a, float b, float c):
+float add(float a, float b):
 	return a + b
 
 
-int subtract(int a, int b):
-	return a - b
+#int draftApi(int ...):
+#	return a - b
 
 
-int divide(int a, int b):
-	return a / b
+int multiply(int a, int b):
+	return a * b
 
