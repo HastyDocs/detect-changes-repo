d08d6a551ab2b69c361b7deb8f41f6aac2ea4c58
mmajde <marek.majde@gmail.com>
2021-12-08 13:45:03 +0100

    Update project_x_api.py

--- project_x_api.py
+++ project_x_api.py
@@ -1,11 +1,15 @@
-float add(float a, float b):
-	return a + b
+float add(float a, float b, float c):
+	return a + b + c
 
 
-#int draftApi(int ...):
-#	return TODO
+int workingApi():
+	return "I am working"
 
 
 int multiply(int a, int b):
 	return a * b
+
+
+int divide(int a, int b):
+	return a / b
 
