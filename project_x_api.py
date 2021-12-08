afc7305c4b49d034a695fdbc0d93c3371502598c
mmajde <marek.majde@gmail.com>
2021-12-08 11:02:38 +0100

    Update project_x_api.py

--- project_x_api.py
+++ project_x_api.py
@@ -2,16 +2,10 @@ float add(float a, float b, float d):
 	return a + b
 
 
-int subtract(int a, int b):
-	return a - b
+#int draftApi(int ...):
+#	return TODO
 
 
 int multiply(int a, int b):
 	return a * b
-
-
-int divide(float a, float b):
-	if b == 0:
-	   raise Exception;
-	return a / b
 
