2d05b3f84a1af51076cc2db5b175353616fa3e27
mmajde <marek.majde@gmail.com>
2021-12-10 19:01:43 +0100

    Update project_x_api.py

--- project_x_api.py
+++ project_x_api.py
@@ -1,15 +1,11 @@
-float add(float a, float b, float c):
-	return a + b + c
+float add(float a, float b):
+	return a + b
 
 
-float subtract(float a, float b):
+float subtract(int a, int b):
 	return a - b
 
 
 int multiply(int a, int b):
 	return a * b
-
-
-int divide(int a, int b):
-   return b \ a
 
